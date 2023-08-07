af6dbd639c444da8a82bd4e666c8dd306791b9b5
Alire7amohebbi <139349183+Alire7amohebbi@users.noreply.github.com>
2023-08-07 15:06:53 +0330

    Add files via upload

--- 07-3/script.js
+++ 07-3/script.js
@@ -1,29 +1,38 @@
-let btn=document.getElementById('btn'), id=document.getElementById('id'),msg=document.getElementById('msg'),str,length,z=0,t1,t2,x='',t3,t4,t5,r;
-btn.onclick = function(){
-    str=id.value;
-    length=str.length;
-    for (let index = 0; index < length; index++) {
-        z=0
-        for (let n = 0; n < 10; n++) {
-            if (str[index]==n){
-                z+=1
-            }
+let id=document.getElementById("id");let msg=document.getElementById("msg");
+let btn=document.getElementById("btn");
+
+btn.onclick=function(){
+    if (id.value.length!=10){
+        msg.innerHTML="invalid"
+    }
+    else{
+        let b;let c=0;
+        let a=id.value.substr(1,9)
+        for (let i=0;i<9;i++){
+            b=Number(a[i])*(i+2)
+            c+=b;
         }
-        t1=(z==0)?false:true;
-    }
-    t3=(str[2]==0 && str[1]==0 && str[0]==0)?false:true;
-    for (let index = 0; index < length; index++) {
-        x+=str[0]
-    }
-    t4=(str==x)?false:true;
-    r=(str[0]*10 + str[1]*9 + str[2]*8 + str[3]*7 + str[4]*6 + str[5]*5 + str[6]*4 + str[7]*3 + str[8]*2)%11;
-    if (r >= 2){
-        r=11-r;
-    }
-    t5=(r==str[9])?true:false;
-    if (t1==false || t3==false || t4==false || t5==false){
-        msg.innerHTML = 'Invalid ID';
-    }else{
-        msg.innerHTML = 'Valid ID';
+        let d=id.value[0];
+        c%=11;
+        if (c<2){
+            c=c
+        }
+        else {
+            c=11-c
+        }
+        if(a[0]=="0" && a[1]=="0" && a[2]=="0"){
+            msg.innerHTML=="invalid";
+        }
+        else if(id.value[0]==id.value[1]==id.value[2]==id.value[3]==id.value[4]==id.value[5]==id.value[6]==id.value[7]==id.value[8]==id.value[9]){
+            msg.innerHTML=="invalid"
+        }
+    
+        else if(d==c){
+            msg.innerHTML="valid"
+        }
+        
+        else{
+            msg.innerHTML="invalid"
+        }
     }
 }
